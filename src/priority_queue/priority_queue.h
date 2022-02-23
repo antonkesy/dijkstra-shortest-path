1214c81f949360613be9b244c27417a9404b7682
Anton Kesy <anton@kesy.de>
2022-02-23 11:17:30 +0100

    add functions

--- src/priority_queue/priority_queue.h
+++ src/priority_queue/priority_queue.h
@@ -1,4 +1,15 @@
 #ifndef DIJKSTRA_SHORTEST_PATH_PRIORITY_QUEUE_H
 #define DIJKSTRA_SHORTEST_PATH_PRIORITY_QUEUE_H
 
+#include <stdint.h>
+#include <stdbool.h>
+
+typedef struct {} priority_queue_t;
+
+void *extract_min(priority_queue_t *queue);
+
+bool insert(priority_queue_t *queue, uint64_t key, void *value);
+
+void *delete(priority_queue_t *queue, uint64_t key);
+
 #endif //DIJKSTRA_SHORTEST_PATH_PRIORITY_QUEUE_H
