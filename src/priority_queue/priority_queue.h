f8018dd99dd41696c2a6cbdc57ed58e590099463
Anton Kesy <anton@kesy.de>
2022-02-23 16:02:52 +0100

    Update priority queue interface

--- src/priority_queue/priority_queue.h
+++ src/priority_queue/priority_queue.h
@@ -4,14 +4,14 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-typedef struct {} priority_queue_t;
+typedef struct {} *priority_queue_t;
 
-priority_queue_t *create_queue(uint64_t capacity);
+priority_queue_t create_queue(uint64_t capacity);
 
-void *extract_min(priority_queue_t *queue);
+void *extract_min(priority_queue_t queue);
 
-bool insert(priority_queue_t *queue, uint64_t key, void *value);
+bool insert(priority_queue_t queue, uint64_t key, void *value);
 
-void *delete(priority_queue_t *queue, uint64_t key);
+bool update_key(priority_queue_t queue, uint64_t key, uint64_t new_value);
 
 #endif //DIJKSTRA_SHORTEST_PATH_PRIORITY_QUEUE_H
