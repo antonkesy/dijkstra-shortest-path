159f09f75389cf1f991ac530c6d1d8c9db167820
Anton Kesy <anton@kesy.de>
2022-02-24 17:57:52 +0100

    Update dijkstra_shortest_path.h

--- include/dijkstra_shortest_path.h
+++ include/dijkstra_shortest_path.h
@@ -6,20 +6,30 @@
 struct node;
 
 typedef struct {
+    struct node *to; //from is clear since edge is from a node
+    uint64_t cost;
+} edge_t;
+
+typedef struct {
     uint64_t count;
-    struct node *neighbours;
+    edge_t *edges;
 } edges_t;
 
 typedef struct node {
-    uint64_t cost;
     void *value;
     edges_t edges;
 } node_t;
 
 typedef struct {
-    node_t dest;
+    uint64_t size;
+    node_t *nodes;
+} graph_t;
+
+typedef struct {
+    node_t *dest;
     uint64_t hops; //amount of nodes/hops from start to dest
     node_t *nodes; //ordered from first to last
+    uint64_t cost;
 } shortest_path_t;
 
 typedef struct {
@@ -27,6 +37,9 @@ typedef struct {
     shortest_path_t *paths;
 } shortest_paths_t;
 
-shortest_paths_t* get_shortest_paths(node_t src);
+//nodes_count: other_nodes + 1
+shortest_paths_t *get_shortest_paths(node_t *src, graph_t *graph);
+
+void delete_shortest_paths(shortest_paths_t *paths);
 
 #endif //DIJKSTRA_SHORTEST_PATH_H
