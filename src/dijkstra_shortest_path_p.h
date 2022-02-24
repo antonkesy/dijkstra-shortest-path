37e5094176ce94eb9f785eaddead0857cef20fef
Anton Kesy <anton@kesy.de>
2022-02-24 17:58:13 +0100

    Create dijkstra_shortest_path_p.h

---
+++ src/dijkstra_shortest_path_p.h
@@ -0,0 +1,12 @@
+#ifndef DIJKSTRA_SHORTEST_PATH_DIJKSTRA_SHORTEST_PATH_P_H
+#define DIJKSTRA_SHORTEST_PATH_DIJKSTRA_SHORTEST_PATH_P_H
+
+#include "../include/dijkstra_shortest_path.h"
+
+shortest_paths_t *create_shortest_paths(const graph_t *graph) ;
+
+uint64_t get_path_costs(shortest_paths_t *paths, node_t *node);
+
+shortest_path_t *get_path(shortest_paths_t *paths, node_t *node);
+
+#endif //DIJKSTRA_SHORTEST_PATH_DIJKSTRA_SHORTEST_PATH_P_H
